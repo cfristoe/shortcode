554dc7fd23bb4a06eba1c1512e7659397fbbd0c3
cfristoe <fristoe82@gmail.com>
2020-06-30 21:15:36 -0400

    First commit of WordPress shortcode plugin

---
+++ hello-world-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Jordan Cry
+ * Description: Share Jordan's crying face with greater ease. 
+ * Version: 0.1
+ * Author: C.Fristoe
+ */
+ function jordan_cry_plugin ($atts) {
+    $Content = "<style>\r\n";
+    $Content .= "h3.testText {r\n";
+    $Content .= "color: #ff3f7e;\r\n";
+    $Content .= "}\r\n";
+    $Content .= "</style>\r\n";
+    $Content .= '<h3 class="testText">HELLO WOOORLD!</h3>'; 
+    $Content .= '<img src= \"https://www.dlf.pt/png/big/5/56420_jordan-cry-face-png.jpg" 
+       alt="jord-avatar" width="120" height="120" class="left-align" />';
+
+return $Content;
+
+}
+
+add_shortcode('jordan_cry', 'jordan_cry_plugin' );
