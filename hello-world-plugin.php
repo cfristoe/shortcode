698473af738774d77ffd1dc64951f5f44d71a8fa
cfristoe <fristoe82@gmail.com>
2020-06-30 22:11:10 -0400

    Image display fixed

--- hello-world-plugin.php
+++ hello-world-plugin.php
@@ -14,7 +14,7 @@
     $Content .= '<h3 class="testText">HELLO WOOORLD!</h3>'; 
     $Content .= '<img src=\"https://www.dlf.pt/png/big/5/56420_jordan-cry-face-png.jpg"
          alt="jord" width="96" height="96" class="left-align" />';
-   }
+   
 
 return $Content;
 
